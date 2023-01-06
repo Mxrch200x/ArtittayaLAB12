6a5c27cb128ea11e226db06e97cf24cfb06c4544
Mxrch200x <gaerdbigsis@gmail.com>
2023-01-06 16:31:52 +0700

    complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,56 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+
+int randgrade(){
+    srand(time(0));
+    int grade = rand()%9;
+    return grade;
+}
+
+
+void Grade(int randgrade){
+    switch(randgrade){
+        case 0:
+            cout << "You will get A in this 261102.";
+            break;
+        case 1:
+            cout << "You will get B+ in this 261102.";
+            break;
+        case 2:
+            cout << "You will get B in this 261102.";
+            break;
+        case 3:
+            cout << "You will get c+ in this 261102.";
+            break;
+        case 4:
+            cout << "You will get C in this 261102."; 
+            break;
+        case 5:
+            cout << "You will get D+ in this 261102.";
+            break;
+        case 6:
+            cout << "You will get D in this 261102.";
+            break;
+        case 7:
+            cout << "You will get F in this 261102.";
+            break;
+        case 8:
+            cout << "You will get W in this 261102."; 
+            break;
+    }
+}
+
+
+int main(){
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    Grade(randgrade());
+    return 0;
+}
